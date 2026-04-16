91877b321f1cc9151a7e3f13eb0630cd42cae22b
Aaron Zhang <98193099+azuda@users.noreply.github.com>
2026-04-16 10:00:23 -0600

    new sheet for PO data + mobile devices

--- upload.py
+++ upload.py
@@ -1,9 +1,10 @@
 # upload.py
 # https://gist.github.com/nahidsaikat/61ecfe9e333bd55fec287e1e04dfbafd#file-csv_to_google_sheet-py
 
-import urllib3
+import csv
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -13,8 +14,18 @@ scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/au
 credentials = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
 client = gspread.authorize(credentials)
 
-spreadsheet = client.open("[csv2sheet] Rundle Computer Report")
+spreadsheet = client.open("[csv2sheet] Rundle Jamf Report")
 
-with open("data/output.csv", "r") as f:
-  content = f.read()
-  client.import_csv(spreadsheet.id, data=content)
+def upload_csv_to_sheet(filepath, tab_name):
+  worksheet = spreadsheet.worksheet(tab_name)
+  worksheet.clear()
+  with open(filepath, "r") as f:
+    data = list(csv.reader(f))
+  worksheet.update(data)
+
+upload_csv_to_sheet("data/output.csv", "Computer Report")
+# upload_csv_to_sheet("data/output2.csv", "Device Report")
+
+# with open("data/output.csv", "r") as f:
+#   content = f.read()
+#   client.import_csv(spreadsheet.id, data=content)
