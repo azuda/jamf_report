a5fe4eade006fc20095ec5e79b66bfeb85e5e375
Aaron Zhang <98193099+azuda@users.noreply.github.com>
2026-04-20 14:20:28 -0600

    add purchasing info + computers report from extattr

--- query_jamf.py
+++ query_jamf.py
@@ -17,7 +17,7 @@ import urllib3
 def get_computers_basic(access_token, token_expiration_epoch):
   access_token, token_expiration_epoch = check_token_expiration(access_token, token_expiration_epoch)
 
-  # GET basic info all computers
+  # GET basic info for all computers
   url = f"{JAMF_URL}/JSSResource/computers/subset/basic"
   headers = {
     "accept": "application/json",
@@ -29,30 +29,68 @@ def get_computers_basic(access_token, token_expiration_epoch):
 def get_computers_userandlocation(access_token, token_expiration_epoch):
   access_token, token_expiration_epoch = check_token_expiration(access_token, token_expiration_epoch)
 
-  # GET userAndLocation all computers
+  # GET computers userAndLocation
   url = f"{JAMF_URL}/api/v2/computers-inventory?section=USER_AND_LOCATION&page=0&page-size=2000&sort=id%3Aasc"
   headers = {
     "accept": "application/json",
     "authorization": f"Bearer {access_token}"
   }
   response = requests.get(url, headers=headers, verify=False)
-
   return response, access_token, token_expiration_epoch
 
-def combine_computers(computers_response, computers_userandlocation_response):
+def get_computers_purchasing(access_token, token_expiration_epoch):
+  access_token, token_expiration_epoch  = check_token_expiration(access_token, token_expiration_epoch)
+
+  # GET computers purchasing info
+  url = f"{JAMF_URL}/api/v2/computers-inventory?section=PURCHASING&page=0&page-size=2000&sort=id%3Aasc"
+  headers = {
+    "accept": "application/json",
+    "authorization": f"Bearer {access_token}"
+  }
+  response = requests.get(url, headers=headers, verify=False)
+  return response, access_token, token_expiration_epoch
+
+def get_computers_extension_attributes(access_token, token_expiration_epoch):
+  access_token, token_expiration_epoch  = check_token_expiration(access_token, token_expiration_epoch)
+
+  # GET computers extension attributes
+  url = f"{JAMF_URL}/api/v2/computers-inventory?section=EXTENSION_ATTRIBUTES&page=0&page-size=2000&sort=id%3Aasc"
+  headers = {
+    "accept": "application/json",
+    "authorization": f"Bearer {access_token}"
+  }
+  response = requests.get(url, headers=headers, verify=False)
+  return response, access_token, token_expiration_epoch
+
+def combine_computers(computers_response, computers_userandlocation_response, computers_purchasing_response, computers_extension_attributes_response):
   computers_json = {}
   computers_json["computers"] = computers_response.json().get("computers", [])
   computers_users_json = computers_userandlocation_response.json().get("results", [])
+  computers_purchasing_json = computers_purchasing_response.json().get("results", [])
+  computers_extension_attributes_json = computers_extension_attributes_response.json().get("results", [])
   total = 0
+
   for c in computers_json["computers"]:
     total += 1
-    c["realname"], c["email"], c["position"] = None, None, None
+    c["realname"], c["email"], c["position"], c["purchase_price"], c["purchase_date"], c["report"] = None, None, None, None, None, None
     for cu in computers_users_json:
       if c["id"] == int(cu["id"]):
         cu_data = cu["userAndLocation"]
         c["realname"] = cu_data["realname"]
         c["email"] = cu_data["email"]
         c["position"] = cu_data["position"]
+        break
+    for cp in computers_purchasing_json:
+      if c["id"] == int(cp["id"]):
+        cp_data = cp["purchasing"]
+        c["purchase_price"] = cp_data["purchasePrice"]
+        c["purchase_date"] = cp_data["poDate"]
+        break
+    for cea in computers_extension_attributes_json:
+      if c["id"] == int(cea["id"]):
+        cea_data = cea["extensionAttributes"]
+        report = [c for c in cea_data if c["name"] == "Rundle Device Report"][0]["values"] if any(c["name"] == "Rundle Device Report" for c in cea_data) else None
+        c["report"] = report[0] if report and len(report) > 0 else None
         break
 
   computers_json["total"] = total
@@ -74,39 +112,56 @@ def get_devices(access_token, token_expiration_epoch):
 def get_devices_userandlocation(access_token, token_expiration_epoch):
   access_token, token_expiration_epoch = check_token_expiration(access_token, token_expiration_epoch)
 
-  # GET userAndLocation all computers
+  # GET mobile devices userAndLocation
   url = f"{JAMF_URL}/api/v2/mobile-devices/detail?section=USER_AND_LOCATION&page=0&page-size=2000&sort=deviceId%3Aasc"
   headers = {
     "accept": "application/json",
     "authorization": f"Bearer {access_token}"
   }
   response = requests.get(url, headers=headers, verify=False)
-
   return response, access_token, token_expiration_epoch
 
 def get_devices_general(access_token, token_expiration_epoch):
   access_token, token_expiration_epoch = check_token_expiration(access_token, token_expiration_epoch)
 
-  # GET general info all mobile devices
+  # GET mobile devices general
   url = f"{JAMF_URL}/api/v2/mobile-devices/detail?section=GENERAL&page=0&page-size=2000&sort=deviceId%3Aasc"
   headers = {
     "accept": "application/json",
     "authorization": f"Bearer {access_token}"
   }
   response = requests.get(url, headers=headers, verify=False)
-
   return response, access_token, token_expiration_epoch
 
-def combine_devices(devices_response, devices_userandlocation_response, devices_general_response):
+def get_devices_purchasing(access_token, token_expiration_epoch):
+  access_token, token_expiration_epoch = check_token_expiration(access_token, token_expiration_epoch)
+
+  # GET mobile devices purchasing
+  url = f"{JAMF_URL}/api/v2/mobile-devices/detail?section=PURCHASING&page=0&page-size=2000&sort=deviceId%3Aasc"
+  headers = {
+    "accept": "application/json",
+    "authorization": f"Bearer {access_token}"
+  }
+  response = requests.get(url, headers=headers, verify=False)
+  return response, access_token, token_expiration_epoch
+
+def combine_devices(devices_response, devices_userandlocation_response, devices_general_response, devices_purchasing_response):
   devices_json = {}
   devices_json["devices"] = devices_response.json().get("mobile_devices", [])
   devices_users_json = devices_userandlocation_response.json().get("results", [])
   devices_general_json = devices_general_response.json().get("results", [])
+  devices_purchasing_json = devices_purchasing_response.json().get("results", [])
 
   total = 0
   for d in devices_json["devices"]:
     total += 1
-    d["date"], d["os"], d["realname"], d["email"], d["position"], d["department"] = None, None, None, None, None, None
+    d["date"], d["os"], d["realname"], d["email"], d["position"], d["department"], d["purchase_price"], d["purchase_date"] = None, None, None, None, None, None, None, None
+    for dg in devices_general_json:
+      if d["id"] == int(dg["mobileDeviceId"]):
+        dg_data = dg["general"]
+        d["date"] = dg_data["lastInventoryUpdateDate"]
+        d["os"] = dg_data["osVersion"]
+        break
     for du in devices_users_json:
       if d["id"] == int(du["mobileDeviceId"]):
         du_data = du["userAndLocation"]
@@ -115,11 +170,11 @@ def combine_devices(devices_response, devices_userandlocation_response, devices_
         d["position"] = du_data["position"]
         d["department"] = du_data["department"]
         break
-    for dg in devices_general_json:
-      if d["id"] == int(dg["mobileDeviceId"]):
-        dg_data = dg["general"]
-        d["date"] = dg_data["lastInventoryUpdateDate"]
-        d["os"] = dg_data["osVersion"]
+    for dp in devices_purchasing_json:
+      if d["id"] == int(dp["mobileDeviceId"]):
+        dp_data = dp["purchasing"]
+        d["purchase_price"] = dp_data["purchasePrice"]
+        d["purchase_date"] = dp_data["poDate"]
         break
 
   devices_json["total"] = total
@@ -143,20 +198,36 @@ def main():
   # get info for all computers
   computers, access_token, token_expiration_epoch  = get_computers_basic(access_token, token_expiration_epoch)
   computers_users, access_token, token_expiration_epoch  = get_computers_userandlocation(access_token, token_expiration_epoch)
-  computers_json = combine_computers(computers, computers_users)
+  computers_purchasing, access_token, token_expiration_epoch = get_computers_purchasing(access_token, token_expiration_epoch)
+  computers_extension_attributes, access_token, token_expiration_epoch = get_computers_extension_attributes(access_token, token_expiration_epoch)
+  computers_json = combine_computers(computers, computers_users, computers_purchasing, computers_extension_attributes)
 
   # get info for all mobile devices
   devices, access_token, token_expiration_epoch = get_devices(access_token, token_expiration_epoch)
   devices_users, access_token, token_expiration_epoch = get_devices_userandlocation(access_token, token_expiration_epoch)
   devices_general, access_token, token_expiration_epoch = get_devices_general(access_token, token_expiration_epoch)
-  devices_json = combine_devices(devices, devices_users, devices_general)
+  devices_purchasing, access_token, token_expiration_epoch = get_devices_purchasing(access_token, token_expiration_epoch)
+  devices_json = combine_devices(devices, devices_users, devices_general, devices_purchasing)
 
-  with open("data/d.json", "w") as f:
+  # raw api responses for debug
+  if not os.path.exists("debug"):
+    os.makedirs("debug")
+  with open("debug/c.json", "w") as f:
+    f.write(json.dumps(computers.json(), indent=2))
+  with open("debug/cu.json", "w") as f:
+    f.write(json.dumps(computers_users.json(), indent=2))
+  with open("debug/cp.json", "w") as f:
+    f.write(json.dumps(computers_purchasing.json(), indent=2))
+  with open("debug/cea.json", "w") as f:
+    f.write(json.dumps(computers_extension_attributes.json(), indent=2))
+  with open("debug/d.json", "w") as f:
     f.write(json.dumps(devices.json(), indent=2))
-  with open("data/du.json", "w") as f:
+  with open("debug/du.json", "w") as f:
     f.write(json.dumps(devices_users.json(), indent=2))
-  with open("data/dg.json", "w") as f:
+  with open("debug/dg.json", "w") as f:
     f.write(json.dumps(devices_general.json(), indent=2))
+  with open("debug/dp.json", "w") as f:
+    f.write(json.dumps(devices_purchasing.json(), indent=2))
 
   # write to file
   if not os.path.exists("data"):
